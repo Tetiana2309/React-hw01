f101df718c022308f36dcb3c3be650c7def169b9
Tetiana2309 <126595700+Tetiana2309@users.noreply.github.com>
2023-09-17 00:26:29 +0200

    ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import user from '../assets/user.json';
+import { Profile } from './Profile/Profile';
+
+import data from '../assets/data.json';
+import { Statistics } from './Statistics/Statistics';
+
+import friends from '../assets/friends.json';
+import { FriendList } from './Friends/FriendList/FriendList';
+
+import transactions from '../assets/transactions.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
+import { GlobalStyle } from '../GlobalStyle';
+import { Box } from './Box';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Box>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+       <GlobalStyle />
+    </Box>
   );
 };
